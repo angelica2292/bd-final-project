9269f3a6bf931c05ad9d9b31494a178b1f6655e8
angelica2292 <35976781+angelica2292@users.noreply.github.com>
2018-03-16 20:20:56 -0600

    Replacing number

--- Test.sql
+++ Test.sql
@@ -4,11 +4,10 @@ DECLARE @number AS decimal(20,10);
 SELECT @name = 'TEST';
 SELECT @name;
 
-<<<<<<< HEAD
 DECLARE @lastName AS varchar(10);
 SELECT @lastName = 'Ulate'
 SELECT @lastName; 
-=======
-SELECT @number = 10;
+
+SELECT @number = 11;
 SELECT @number;
->>>>>>> 4805d502741f01621ff99a4711a5ba59ce33d0ba
+
