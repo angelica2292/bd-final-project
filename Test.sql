a8f86b9f1d59ec5682191770d172de6750512b90
jgabrielgv <jgmezvargas@gmail.com>
2018-03-16 20:19:28 -0600

    Editing variable

--- Test.sql
+++ Test.sql
@@ -9,6 +9,6 @@ DECLARE @lastName AS varchar(10);
 SELECT @lastName = 'Ulate'
 SELECT @lastName; 
 =======
-SELECT @number = 10;
+SELECT @number = 12;
 SELECT @number;
 >>>>>>> 4805d502741f01621ff99a4711a5ba59ce33d0ba
